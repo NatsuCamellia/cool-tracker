776deb12eff9c2b5c0973e4ab4679dfad492a2c5
NatsuCamellia <natsucamellia@gmail.com>
2025-09-01 23:16:50 +0800

    Update version to 0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "net.natsucamellia.cooltracker"
         minSdk = 29
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
